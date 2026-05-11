d1a9c8d897151232a81a3ced11d8e21e07a2d98e
Skull-coder <skullthecoder@gmail.com>
2026-05-11 18:26:02 +0530

    Fix navigation bug

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Sidebar from "../layout/Sidebar";
 import "./Dashboard.css";
 
 const Dashboard = () => {
+  const accessToken = localStorage.getItem("accessToken");
+
+  if (!accessToken) {
+    return <Navigate to="/login" replace />;
+  }
+
   return (
     <div className="dashboard-layout">
       <Sidebar />
